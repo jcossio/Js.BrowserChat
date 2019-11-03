c3735118c3f03c4d3250d62a2b4b3d1daef4d91e
Jorge Cossio <jorge.cossio@yahoo.com>
2019-11-03 16:19:38 -0500

    Change project to MVC

---
+++ Js.BrowserChat.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Js.BrowserChat.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
